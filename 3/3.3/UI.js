03ae535ec39d0fc05c02f116c765a00e91cdbced
ICX\aliaksei.zhukouski <zhukovskyalexey94@gmail.com>
2017-07-20 13:33:56 +0300

    3.3 done

---
+++ 3/3.3/UI.js
@@ -0,0 +1,37 @@
+(function(){
+
+    var click = function(){
+
+        var originStringArray = document.getElementById("originArray").value;
+        var sortMethod = document.getElementById("sortMethod").value;
+
+        var arraySorter = new ArraySorter();
+
+        if (sortMethod == 1)
+        {
+            var sortedArray = arraySorter.bableSort(originStringArray);
+        }
+        else if (sortMethod == 2)
+        {
+            var sortedArray = arraySorter.quickSort(originStringArray);
+        }
+        else if (sortMethod == 3)
+        {
+            var sortedArray = arraySorter.shellSort(originStringArray);
+        }
+        else if (sortMethod == 4)
+        {
+            var sortedArray = arraySorter.countingSort(originStringArray);
+        }
+        
+        
+        console.log(sortedArray, sortMethod);
+
+    }
+
+    document.getElementById("btn").onclick = click;
+
+})();
+
+
+
