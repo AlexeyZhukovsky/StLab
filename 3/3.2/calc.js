99ca6ab89f8e242961aaee112ee9d42bbed6f2bb
ICX\aliaksei.zhukouski <zhukovskyalexey94@gmail.com>
2017-07-21 15:05:59 +0300

    3.2 done

--- 3/3.2/calc.js
+++ 3/3.2/calc.js
@@ -8,56 +8,89 @@ var Calc = function()
 // var sumPattern = /\d+\+\d+/;
 // var subPattern = /\d+\-\d+/;
 
+
+// regForFloat /\d+\.\d+\/\d+\.\d+|\d+\/\d+/g
+// /\d+\.\d+[*,/]\d+\.\d+|\d+[*,/]\d+/
+// \d+\.\d+[*,/]\d+|\d+[*,/]\d+\.\d+|\d+[*,/]\d+|\d+\.\d+[*,/]\d+\.\d+
+// \d+\.\d+\/\d+\.\d+|\d+\.\d+\/\d+|\d+\/\d+\.\d+|\d+\/\d+
+
 mulAndDivSearch = function(str){
 this.str = str;
-mulAndDiv = str.match(/\d+[*,/]\d+/);
+mulAndDiv = str.match(/\d+\.\d+[*,/]\d+\.\d+|\d+\.\d+[*,/]\d+|\d+[*,/]\d+\.\d+|\d+[*,/]\d+/);
+// mulAndDiv = str.match(/\d+\.\d+[*,/]\d+|\d+[*,/]\d+\.\d+|\d+[*,/]\d+|\d+\.\d+[*,/]\d+\.\d+/);
+// mulAndDiv = str.match(/\d+\.\d+[*,/]\d+\.\d+|\d+[*,/]\d+/);
+// mulAndDiv = str.match(/\d+[*,/]\d+/);
 if (mulAndDiv != null){
     if (/\*/.test(mulAndDiv))
         {
             s = mulAndDiv[0].split(/\*/);
             r = s[0]*s[1];
-            str = str.replace(/\d+\*\d+/,r);
+            str = str.replace(/\d+\.\d+\*\d+\.\d+|\d+\.\d+\*\d+|\d+\*\d+\.\d+|\d+\*\d+/,r);
+            // str = str.replace(/\d+\.\d+\*\d+|\d+\*\d+\.\d+|\d+\*\d+|\d+\.\d+\*\d+\.\d+/,r);
+            // str = str.replace(/\d+\.\d+\*\d+\.\d+|\d+\*\d+/,r);
+            // str = str.replace(/\d+\*\d+/,r);
         }
         else 
         {
             s = mulAndDiv[0].split(/\//);
             r = s[0]/s[1];
-            str = str.replace(/\d+\/\d+/,r);
+            str = str.replace(/\d+\.\d+\/\d+\.\d+|\d+\.\d+\/\d+|\d+\/\d+\.\d+|\d+\/\d+/,r);
+            // str = str.replace(/\d+\.\d+\/\d+|\d+\/\d+\.\d+|\d+\/\d+|\d+\.\d+\/\d+\.\d+/,r);
+            // str = str.replace(/\d+\.\d+\/\d+\.\d+|\d+\/\d+/,r);
+            // str = str.replace(/\d+\/\d+/,r);
         }
        return mulAndDivSearch(str);
 }
-return;
+return str;
 };
 
 
 
 sumAndSubSearch = function(str){
 this.str = str;
-sumAndSub = str.match(/\d+[+,-]\d+/);
+sumAndSub = str.match(/\d+\.\d+[+,-]\d+\.\d+|\d+\.\d+[+,-]\d+|\d+[+,-]\d+\.\d+|\d+[+,-]\d+/);
+// sumAndSub = str.match(/\d+[+,-]\d+/);
 if (sumAndSub != null){
     if (/\+/.test(sumAndSub))
         {
             s = sumAndSub[0].split(/\+/);
             r = (+s[0])+(+s[1]);
-            str = str.replace(/\d+\+\d+/,r);
+            str = str.replace(/\d+\.\d+\+\d+\.\d+|\d+\.\d+\+\d+|\d+\+\d+\.\d+|\d+\+\d+/,r);
+            // str = str.replace(/\d+\+\d+/,r);
         }
         else 
         {
             s = sumAndSub[0].split(/\-/);
             r = (+s[0])-(+s[1]);
-            str = str.replace(/\d+\-\d+/,r);
+            str = str.replace(/\d+\.\d+\-\d+\.\d+|\d+\.\d+\-\d+|\d+\-\d+\.\d+|\d+\-\d+/,r);
+            // str = str.replace(/\d+\-\d+/,r);
         }
         
        return sumAndSubSearch(str);
 }
-return;
+return str;
 };
+
+bracketsSearch = function(str)
+{
+    this.str = str;
+    // bracketsExp = str.match(/\([0-9.,\+\-\*\/]+\)/);
+    bracketsExp = str.match(/\(([^()]*)\)/);
+    if (bracketsExp != null)
+        {
+            var calculateOfBrackets = self.calculate(bracketsExp[1]);
+            str = str.replace(/\([0-9.,\+\-\*\/]+\)/,calculateOfBrackets);
+            return bracketsSearch(str);   
+        }
+    return str;    
+}
 
 self.calculate = function(str)
 {   
     this.str = str;
-    mulAndDivSearch(str);
-    sumAndSubSearch(str);
+    str = bracketsSearch(str);
+    str = mulAndDivSearch(str);
+    str = sumAndSubSearch(str);
     return str;
 };
 
